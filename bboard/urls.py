43fef5b7dffb1ca2854a4fd808eee0cfe33dd2c0
AndyVorzkla <andreyshadrin007@gmail.com>
2022-07-09 12:31:27 +0300

    Добавил форму, базовый шаблон

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 app_name = 'bboard'
 
 urlpatterns = [
-    path('<int:rubric_id>', by_rubric, name='by_rubric'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('add/', BbCreateView.as_view(), name='add'),
     path('', index, name='index'),
 
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.urls import reverse_lazy as _
+from django.views.generic import CreateView
 
+from .forms import BbForm
 from .models import Bb, Rubric
 
 
@@ -27,6 +30,17 @@ def by_rubric(request, rubric_id: int):
     }
 
     return render(request, 'bboard/by_rubric.html', context=context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = _('bboard:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 # def index(request):
 #     s = 'Список объявлений\r\n\r\n\r\n'
